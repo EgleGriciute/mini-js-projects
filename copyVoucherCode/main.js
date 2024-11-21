575d1402ba3cc6fa2ad011abdf9b9a266d2e796f
Egle Griciute <egllegriciute@gmail.com>
2024-11-21 14:06:48 +0200

    implementation

---
+++ copyVoucherCode/main.js
@@ -0,0 +1,19 @@
+//  Select input and copy button elements form DOM
+
+const copyBtn = document.querySelector(".btn");
+const inputText = document.querySelector("input");
+
+copyBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  // navigator.clipboard.writeText() copies the text content from the input field (inputText.value) to the clipboard:
+
+  navigator.clipboard.writeText(inputText.value).then(() => {
+    copyBtn.innerText = "Copied";
+    copyBtn.style.color = "green";
+    setTimeout(() => {
+      copyBtn.innerText = "Copy";
+      copyBtn.style.color = "black";
+    }, 3000);
+  });
+});
---
+++ counterApp/main.js
@@ -0,0 +1,42 @@
+// Select DOM elements
+const counter = document.querySelector(".count");
+const allBtns = document.querySelectorAll(".buttonsContainer");
+
+const subtractBtn = allBtns[0].children[0];
+const resetBtn = allBtns[0].children[1];
+const addBtn = allBtns[0].children[2];
+
+// Counter output should change in accordance to a result:
+
+function updateCounterColor() {
+  const count = parseInt(counter.textContent);
+
+  switch (true) {
+    case count > 0:
+      counter.style.color = "green";
+      break;
+    case count < 0:
+      counter.style.color = "red";
+      break;
+    default:
+      counter.style.color = "white";
+      break;
+  }
+}
+
+// ExecutE functionality:
+
+subtractBtn.addEventListener("click", () => {
+  counter.textContent--;
+  updateCounterColor();
+});
+
+resetBtn.addEventListener("click", () => {
+  counter.textContent = 0;
+  updateCounterColor();
+});
+
+addBtn.addEventListener("click", () => {
+  counter.textContent++;
+  updateCounterColor();
+});
